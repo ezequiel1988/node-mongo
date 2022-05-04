9ba69c28430c9d9985393cfa118c23061ee320ad
ezequiel <ekiolivier@gmail.com>
2022-05-04 19:05:56 -0300

    fix

--- src/Controller/upload.controller.js
+++ src/Controller/upload.controller.js
@@ -5,10 +5,12 @@ const { IMAGE_SAVED_SUCCESSFULLY } = require('../Constants/messages.constant');
 
 const upload = async (req, res) => {
     try {
+        
         const {user_id, file} = req;
         const upload = await cloudinary.uploader.upload(file.path)
         await User.updateOne({_id: user_id}, {avatar: upload.secure_url});
         res.status(200).json({ message: IMAGE_SAVED_SUCCESSFULLY })
+
     } catch (error) {
         console.log(error);
         res.status(500).send({error});
